84d251f8c61fa1a7474a68239cede4f242ae4bbf
RadosavPanic <radosav.panic01@gmail.com>
2023-08-15 08:42:59 +0200

    added mocks and spies to the test

--- script.js
+++ script.js
@@ -16,6 +16,4 @@ const googleSearch = (searchInput, db) => {
   return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-console.log(googleSearch("cat", googleDatabase));
-
 module.exports = googleSearch;
